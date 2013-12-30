cda27df3b085b48089f57076efa7f9b5fa3d00fc
Aaron Shumway <aclshumway@gmail.com>
2013-12-30 12:16:54 -0700

    lazy load account and survey panel dependencies

--- app/panels/vm.panel.accounts.js
+++ app/panels/vm.panel.accounts.js
@@ -1,21 +1,32 @@
 define('src/panels/vm.panel.accounts', [
+  'src/core/helpers',
   'src/core/notify',
   'src/core/utils',
   'src/core/vm.controller',
-  'src/account/vm.account.search',
-  'src/account/vm.account.checklist',
-  'src/account/vm.account'
 ], function(
+  helpers,
   notify,
   utils,
-  ControllerViewModel,
-  SearchAccountViewModel,
-  ChecklistAccountViewModel,
-  AccountViewModel
+  ControllerViewModel
 ) {
   "use strict";
 
-  var childRoutePart = 'accountid',
+  // lazy load account dependencies
+  var deps = {},
+    ensureDeps = helpers.onetimer(function loadDeps(cb) {
+      require([
+        'src/account/vm.account.search',
+        'src/account/vm.account.checklist',
+        'src/account/vm.account',
+      ], function() {
+        var args = arguments;
+        deps.SearchAccountViewModel = args[0];
+        deps.ChecklistAccountViewModel = args[1];
+        deps.AccountViewModel = args[2];
+        cb();
+      });
+    }),
+    childRoutePart = 'accountid',
     newCount = 0;
 
   function AccountsPanelViewModel(options) {
@@ -40,7 +51,7 @@ define('src/panels/vm.panel.accounts', [
     };
     _this.clickNew = function() {
       newCount++;
-      var vm = new ChecklistAccountViewModel({
+      var vm = new deps.ChecklistAccountViewModel({
         controller: _this,
         routePart: childRoutePart,
         id: 'new' + newCount,
@@ -58,29 +69,32 @@ define('src/panels/vm.panel.accounts', [
   AccountsPanelViewModel.prototype.onLoad = function(routeData, join) { // overrides base
     var _this = this,
       id = routeData[childRoutePart],
+      cb = join.add(),
       vm;
 
-    _this.searchVM = new SearchAccountViewModel({
-      routePart: childRoutePart,
-      id: 'search',
-      title: 'Search',
-    });
+    ensureDeps(function() {
+      _this.searchVM = new deps.SearchAccountViewModel({
+        routePart: childRoutePart,
+        id: 'search',
+        title: 'Search',
+      });
 
-    _this.list([
-      createAccountVM(100001, '100001'),
-      createAccountVM(100002, '100002'),
-      createAccountVM(100003, '100003'),
-    ]);
+      _this.list([
+        createAccountVM(100001, '100001'),
+        createAccountVM(100002, '100002'),
+        createAccountVM(100003, '100003'),
+      ]);
 
-    /* jshint eqeqeq:false */
-    if (id && parseInt(id, 10) == id) {
-      vm = _this.findChild(routeData);
-      if (!vm) {
-        vm = createAccountVM(parseInt(id, 10), id);
-        _this.list.push(vm);
+      /* jshint eqeqeq:false */
+      if (id && parseInt(id, 10) == id) {
+        vm = _this.findChild(routeData);
+        if (!vm) {
+          vm = createAccountVM(parseInt(id, 10), id);
+          _this.list.push(vm);
+        }
       }
-    }
-    join.add()();
+      cb();
+    });
   };
 
   AccountsPanelViewModel.prototype.findChild = function(routeData) {
@@ -98,7 +112,7 @@ define('src/panels/vm.panel.accounts', [
     var _this = this,
       id;
 
-    if (vm instanceof SearchAccountViewModel) {
+    if (vm instanceof deps.SearchAccountViewModel) {
       id = "search";
       // } else if (vm instanceof NewAccountViewModel) {
       //   id = "new";
@@ -112,7 +126,7 @@ define('src/panels/vm.panel.accounts', [
   };
 
   function createAccountVM(id, name) {
-    return new AccountViewModel({
+    return new deps.AccountViewModel({
       routePart: childRoutePart,
       id: id,
       title: name,
--- app/panels/vm.panel.surveys.js
+++ app/panels/vm.panel.surveys.js
@@ -1,25 +1,37 @@
 define('src/panels/vm.panel.surveys', [
-  'src/core/vm.layers',
-  'src/survey/vm.tokens',
-  'src/survey/vm.possibleanswers',
-  'src/survey/vm.surveytype',
   'ko',
   'src/dataservice',
+  'src/core/vm.layers',
+  'src/core/helpers',
   'src/core/notify',
   'src/core/utils',
   'src/core/vm.controller',
 ], function(
-  LayersViewModel,
-  TokensViewModel,
-  PossibleAnswersViewModel,
-  SurveyTypeViewModel,
   ko,
   dataservice,
+  LayersViewModel,
+  helpers,
   notify,
   utils,
   ControllerViewModel
 ) {
   'use strict';
+
+  // lazy load account dependencies
+  var deps = {},
+    ensureDeps = helpers.onetimer(function loadDeps(cb) {
+      require([
+        'src/survey/vm.tokens',
+        'src/survey/vm.possibleanswers',
+        'src/survey/vm.surveytype',
+      ], function() {
+        var args = arguments;
+        deps.TokensViewModel = args[0];
+        deps.PossibleAnswersViewModel = args[1];
+        deps.SurveyTypeViewModel = args[2];
+        cb();
+      });
+    });
 
   function SurveysPanelViewModel(options) {
     var _this = this;
@@ -48,39 +60,43 @@ define('src/panels/vm.panel.surveys', [
 
   SurveysPanelViewModel.prototype.onLoad = function(routeData, join) { // overrides base
     var _this = this,
-      tokensVM = new TokensViewModel(),
-      possibleAnswersVM = new PossibleAnswersViewModel(),
+      tokensVM, possibleAnswersVM,
       cb = join.add(),
       depJoin = join.create();
 
-    tokensVM.load(routeData, depJoin.add());
-    possibleAnswersVM.load(routeData, depJoin.add());
+    ensureDeps(function() {
+      tokensVM = new deps.TokensViewModel();
+      possibleAnswersVM = new deps.PossibleAnswersViewModel();
 
-    dataservice.survey.surveyTypes.read({}, null, function(err, resp) {
-      if (err) {
-        return cb(err);
-      }
-      // wait until the dependencies have been loaded
-      depJoin.when(function(err) {
-        utils.safeCallback(err, function() {
-          if (resp.Value) {
-            var list = resp.Value.map(function(model) {
-              var vm = new SurveyTypeViewModel({
-                controller: _this.controller,
-                layersVM: _this.layersVM,
-                tokensVM: tokensVM,
-                possibleAnswersVM: possibleAnswersVM,
-                model: model,
+      tokensVM.load(routeData, depJoin.add());
+      possibleAnswersVM.load(routeData, depJoin.add());
+
+      dataservice.survey.surveyTypes.read({}, null, function(err, resp) {
+        if (err) {
+          return cb(err);
+        }
+        // wait until the dependencies have been loaded
+        depJoin.when(function(err) {
+          utils.safeCallback(err, function() {
+            if (resp.Value) {
+              var list = resp.Value.map(function(model) {
+                var vm = new deps.SurveyTypeViewModel({
+                  controller: _this.controller,
+                  layersVM: _this.layersVM,
+                  tokensVM: tokensVM,
+                  possibleAnswersVM: possibleAnswersVM,
+                  model: model,
+                });
+                vm.load(routeData, join.add());
+                return vm;
               });
-              vm.load(routeData, join.add());
-              return vm;
-            });
-            _this.surveyTypes(list);
-          } else {
-            //
-            _this.surveyTypes([]);
-          }
-        }, cb);
+              _this.surveyTypes(list);
+            } else {
+              //
+              _this.surveyTypes([]);
+            }
+          }, cb);
+        });
       });
     });
   };
