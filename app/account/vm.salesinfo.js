f267c3ef34a6e30a05c78590d44e3e122d8ce701
Andres Sosa <ASosa@SecuritysSciences.com>
2014-01-08 10:34:54 -0700

    Added elements to the Sales Info Screen.

--- app/account/vm.salesinfo.js
+++ app/account/vm.salesinfo.js
@@ -21,6 +21,9 @@ define('src/account/vm.salesinfo', [
 
     // ** Fields
     _this.psComboVM = new ComboViewModel();
+    _this.ctComboVM = new ComboViewModel();
+    _this.apckComboVM = new ComboViewModel();
+    _this.clComboVM = new ComboViewModel();
     _this.title = ko.observable(_this.title);
 
     //
@@ -31,8 +34,11 @@ define('src/account/vm.salesinfo', [
   SalesInfoViewModel.prototype.viewTmpl = 'tmpl-salesinfo';
 
   SalesInfoViewModel.prototype.onLoad = function(routeData, join) { // overrides base
+    // ** Init
     var _this = this,
       cb = join.add();
+
+    // ** Pull pointsystems
     dataservice.salessummary.pointsystems.read({}, null, function(err, resp) {
       utils.safeCallback(err, function() {
         // ** Bind data
@@ -43,6 +49,48 @@ define('src/account/vm.salesinfo', [
           };
         });
         _this.psComboVM.setList(map);
+      }, cb);
+    });
+
+    // ** Pull Cellular Types
+    dataservice.salessummary.cellulartypes.read({}, null, function(err, resp) {
+      utils.safeCallback(err, function() {
+        // ** Bind data
+        var map = resp.Value.map(function(item) {
+          return {
+            text: item.CellularTypeName,
+            value: item.CellularTypeID
+          };
+        });
+        this.ctComboVM.setList(map);
+      }, cb);
+    });
+
+    // ** Pull alarm.com packages
+    dataservice.salessummary.vendoralarmcompacakges.read({}, function(err, resp) {
+      utils.safeCallback(err, function() {
+        // ** Bind Data
+        var map = resp.Value.map(function(item) {
+          return {
+            text: item.PackageName,
+            value: item.AlarmComPackageID
+          };
+        });
+        this.apckComboVM.setList(map);
+      }, cb);
+    });
+
+    // ** Contract Length
+    dataservice.salessummary.contractlengthsget.read({}, function(err, resp) {
+      utils.safeCallback(err, function() {
+        // Bind data
+        var map = resp.Value.map(function(item) {
+          return {
+            text: item.ContractName,
+            value: item.ContractTemplateID
+          };
+        });
+        this.clComboVM.setList(map);
       }, cb);
     });
   };
