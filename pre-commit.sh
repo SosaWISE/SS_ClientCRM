fadd24778c476764453d92a587ff23e50b6aaacd
Aaron Shumway <aclshumway@gmail.com>
2014-03-04 13:49:58 -0700

    added pre-commit and setup

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,21 +8,50 @@
 
 exec 1>&2
 
-# only test code that is being committed
-git stash -q --keep-index
 
+# The below check is to make sure file names don't include unusual characters.
+# It was modified from the example pre-commit hook found in any recently created repo.
+NEW_NAMES=$(git diff --cached --name-only --diff-filter=ACR -z)
+if echo "${NEW_NAMES}" | grep -q [^a-zA-Z0-9\.\_\\\/\-]; then
+  echo "Error: Attempt to add an invalid file name"
+  echo "${NEW_NAMES}" | grep [^a-zA-Z0-9\.\_\\\/\-]
+  echo
+  echo "File names can only include letters, numbers,"
+  echo "hyphens, underscores and periods"
+  echo
+  echo "Please rename your file before committing it"
+  exit 1
+fi
+
+
+# only test code that is being committed
+git stash save --include-untracked --keep-index --quiet "pre-commit"
+function cleanup {
+  # pop stash
+  git stash pop --quiet --index
+}
+# cleanup on exit
+trap cleanup EXIT
 
 
 # jshint
-#TODO: actually jshint
+RESULT=`grunt --no-color jshint`
+if echo "${RESULT}" | grep -q "Aborted"; then
+  echo "JsHint failed"
+  echo
+  echo "${RESULT}"
+  exit 1
+fi
 
-# jsformatted
-#TODO: actually test if formatted correctly
-
-
-
-# pop stash
-git stash pop -q
+# javascript formatting
+RESULT=`grunt --no-color jsbeautifier:test`
+if echo "${RESULT}" | grep -q "Aborted"; then
+  echo "Javascript formatting test failed"
+  echo "One or more javascript files is not correctly formatted"
+  echo
+  echo "${RESULT}"
+  exit 1
+fi
 
 
 exit 0
