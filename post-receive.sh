23c22062a372d92602e3b4ae226a325afcad7f3e
Aaron Shumway <aclshumway@gmail.com>
2014-04-24 15:56:36 -0600

    done testing deploy to production server

--- post-receive.sh
+++ post-receive.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# remote: fatal: Could not jump back into original cwd: No such file or directory
+# crmprod works, but crm doesn't since it is crm.git without .git - http://stackoverflow.com/a/8513916
 DIR=/home/user/crmprod
 
 # make output dir
