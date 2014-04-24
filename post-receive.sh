b8d2054d82a7b7a20129e39315f6207b6ad7ca6b
Aaron Shumway <aclshumway@gmail.com>
2014-04-24 15:38:12 -0600

    testing git deploy

--- post-receive.sh
+++ post-receive.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-DIR=/home/dev/src/staging/crm
+DIR=/home/user/crm
 
 # make output dir
 mkdir -p ${DIR}
 # checkout tree
-GIT_WORK_TREE=${DIR} git checkout -f master
+# GIT_WORK_TREE=${DIR} git checkout -f master
+git --git-dir=/home/user/crm.git --work-tree=${DIR} checkout -f master
+
 # install and build
 cd ${DIR}
 npm install
