db5fb8f0fb599a858e7b4c6e7f98798c95960450
Aaron Shumway <aclshumway@gmail.com>
2014-05-20 14:31:35 -0500

    once again, got swing panel to pass `grunt precommit`

--- app/panels/swing.panel.vm.js
+++ app/panels/swing.panel.vm.js
@@ -276,12 +276,12 @@ define('src/panels/swing.panel.vm', [
 
     var msAccountSearch = vm.data.getValue();
 
-    console.log('vm data value:'+JSON.stringify(msAccountSearch));
-    console.log('Swing Equipment value: '+msAccountSearch.SwingEquipment);
+    console.log('vm data value:' + JSON.stringify(msAccountSearch));
+    console.log('Swing Equipment value: ' + msAccountSearch.SwingEquipment);
 
     dataservice.swingaccountsrv.CustomerSwingInterim.post(null, {
       InterimAccountId: msAccountSearch.AccountID,
-      SwingEquipment : msAccountSearch.SwingEquipment
+      SwingEquipment: msAccountSearch.SwingEquipment
     }, null, utils.safeCallback(null, function(err, resp) {
 
       //debugging
