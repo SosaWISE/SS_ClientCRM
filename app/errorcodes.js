3c18b4482a2e5fa56ddda9da61e62dc4eb828f8e
Andres Sosa <ASosa@SecuritysSciences.com>
2014-07-02 17:07:02 -0600

    Added new error code for NULL Exceptions in SQL

--- app/errorcodes.js
+++ app/errorcodes.js
@@ -9,6 +9,7 @@ define('src/errorcodes', [], function() {
 
     '70110': 'Item was not found',
     '70120': 'Duplicate item found',
+    '70130': 'Null exception occurred',
 
     '990000': 'Connection Refused',
     '990001': 'Error processing response',
