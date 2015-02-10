db90b1bfb943adae014dd326acfec64c3b02f4f6
Aaron Shumway <aclshumway@gmail.com>
2015-02-10 13:31:50 -0600

    deploy.sh: use rsync instead of cp

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 shopt -s extglob
 
-grunt init
+# grunt init
 grunt build
 
-echo "Copy to ${DEST}"
-# remove all but webconfig from destination
-rm -rf "${DEST}/!(webconfig.js)"
-# copy all but webconfig to destination
-mkdir -p "${DEST}"
-cp -r www/!(webconfig.js) "${DEST}"
+echo "rsync to ${DEST}"
+rsync -av --delete --exclude /webconfig.js ./www/ "${DEST}"
 echo "...done"
