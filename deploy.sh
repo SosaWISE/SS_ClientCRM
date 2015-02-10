c2ee1792a81f5b64801958e0aa32ef984d845b24
Aaron Shumway <aclshumway@gmail.com>
2015-02-09 18:10:36 -0600

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+shopt -s extglob
+
+grunt init
+grunt build
+
+echo "Copy to dest..."
+# remove all but webconfig from destination
+rm -rf "${DEST}/!(webconfig.js)"
+# copy all but webconfig to destination
+mkdir -p "${DEST}"
+cp -r www/!(webconfig.js) "${DEST}"
+echo "...done"
