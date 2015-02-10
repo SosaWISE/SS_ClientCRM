38b10fad8e28c4cecb6ba1ab8db6adec97c0a5af
Aaron Shumway <aclshumway@gmail.com>
2015-02-10 13:36:46 -0600

    deploy.sh: ensure dest exists

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@
 shopt -s extglob
 
 # grunt init
-grunt build
+grunt build --no-color
 
 echo "rsync to ${DEST}"
+# ensure dest exists
+mkdir -p "${DEST}"
+# copy/delete
 rsync -av --delete --exclude /webconfig.js ./www/ "${DEST}"
 echo "...done"
