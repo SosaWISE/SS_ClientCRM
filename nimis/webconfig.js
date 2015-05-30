1ddfd0d17fb125273969607e12c0839a2aef260c
Andres Sosa <asosa@nexsense.com>
2015-05-30 15:30:30 -0600

    adding file because stage is broken.

---
+++ nimis/webconfig.js
@@ -0,0 +1,8 @@
+define('webconfig', [], function() {
+  "use strict";
+  return {
+    useMocks: false,
+    // serviceDomain: 'sse.services.CmsCORS',
+    serviceDomain: 'cs1.stg.nexsense.com',
+  };
+});
