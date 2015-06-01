11cc945fcd723e40ffde8757e3494e5301d04c61
Aaron Shumway <aclshumway@gmail.com>
2015-06-01 12:22:28 -0500

    remove environment specific webconfigs

--- nimis/webconfig.js
+++
@@ -1,8 +0,0 @@
-define('webconfig', [], function() {
-  "use strict";
-  return {
-    useMocks: false,
-    // serviceDomain: 'sse.services.CmsCORS',
-    serviceDomain: 'cs1.stg.nexsense.com',
-  };
-});
