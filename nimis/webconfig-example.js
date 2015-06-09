e918fe1cd98ccf485555e8d109a6b5ee602f7688
Andres Sosa <asosa@nexsense.com>
2015-06-08 22:29:34 -0600

    fixing issues with webconfig.js.

--- nimis/webconfig-example.js
+++ nimis/webconfig-example.js
@@ -2,5 +2,11 @@ define('webconfig', [], function() {
   "use strict";
   return {
     useMocks: false,
+    serviceDomain: 'sse.services.CmsCORS',
+    //serviceDomain: 'cs1.stg.nexsense.com',
+    salesInfosMax: {
+      v01: 191208,
+      v02: 999999,
+    },
   };
 });
