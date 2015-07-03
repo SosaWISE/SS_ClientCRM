d5046026b331dfcc763d81e40bdf84f349be932e
Andres Sosa <asosa@nexsense.com>
2015-07-03 04:11:44 -0600

    Added new My Accounts report.

--- app/salesreports/myaccounts.report.vm.js
+++ app/salesreports/myaccounts.report.vm.js
@@ -24,6 +24,21 @@ define("src/salesreports/myaccounts.report.vm", [
     _this.filterRange.subscribe(function(newValue) {
       console.log("New Value: ", newValue);
       /** Figure out the date ranges. */
+      switch (newValue) {
+        case "thisWeek":
+          setThisWeek(_this);
+          break;
+        case "today":
+          setToday(_this);
+          break;
+        case "thisMonth":
+          setThisMonth(_this);
+          break;
+        case "allTime":
+          setAllTime(_this);
+          break;
+
+      }
 
     });
 
@@ -62,10 +77,55 @@ define("src/salesreports/myaccounts.report.vm", [
 
   MyAccountsReportViewModel.prototype.onLoad = function(routeData, extraData, join) { // overrides base
     var _this = this;
+
+    setThisWeek(_this);
     _this.cmdGo.execute(join.add());
 
     join.add()();
   };
+
+  function setThisWeek(_this) {
+    var d = new Date(),
+      today = d.getDay(),
+      startDate = new Date(),
+      endDate = new Date();
+
+    startDate.setDate(d.getDate() + ((-1) * today));
+    endDate.setDate(d.getDate() + (6 - today));
+
+    _this.data.startDate(startDate);
+    _this.data.endDate(endDate);
+
+  }
+
+  function setToday(_this) {
+    var startDate = new Date(),
+      endDate = new Date();
+
+    _this.data.startDate(startDate);
+    _this.data.endDate(endDate);
+
+  }
+
+  function setThisMonth(_this) {
+    var startDate = new Date(),
+      endDate = new Date();
+
+    // startDate = new Date((startDate.getMonth() + 1) + '/' + '1/' + startDate.getFullYear());
+    startDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
+    _this.data.startDate(startDate);
+    endDate = new Date(endDate.getFullYear(), endDate.getMonth() + 1, 0);
+    _this.data.endDate(endDate);
+  }
+
+  function setAllTime(_this) {
+    var startDate = new Date(),
+      endDate = new Date();
+
+    startDate = new Date(2013, 1, 1);
+    _this.data.startDate(startDate);
+    _this.data.endDate(endDate);
+  }
 
   function loadMyAccountsReport(_this, cb) {
     if (!_this.data.isValid.peek()) {
